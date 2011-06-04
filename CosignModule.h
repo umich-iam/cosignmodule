55ee9cbc105dc08601d9c297fb3e10a74347e667
Jarod Malestein <jarod@umich.edu>
2011-06-04 07:19:38 +0000

    Version number update.

--- CosignModule.h
+++ CosignModule.h
@@ -18,7 +18,7 @@
 #include "snetpp.h"
 */
 
-static std::string VERSION = "3.1.0 RC1, build 9";
+static std::string VERSION = "3.1.0 RC1";
 
 enum PROTECTEDSTATUS { cosignUnprotected, cosignProtected, cosignAllowPublicAccess };
 
